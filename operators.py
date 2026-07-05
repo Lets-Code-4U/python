38235d3ed0b4e9c2b8c683358be9e18acec5fbff
Lets-Code-4U <sarveshletscode4u@gmail.com>
2026-07-05 23:21:48 +0530

    arithmetic operators

---
+++ operators.py
@@ -0,0 +1,12 @@
+# Operators in python
+
+#Arithmetic operators
+a = 43
+b = 34
+
+print("Addition :", a + b)
+print("Subtraction :", a - b)
+print("Multiplication :", a * b)
+print("Division :", a / b)
+print("Modulous :", a % b)  # Returns the remainder left after division
+print("Exponential :", 2**4)  # it will multiply 2 four times as 2 to the power 4
